bea363abedf84fbb8f494f60d3c5d27b0c0278ef
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-08-09 15:51:51 +0400

    Added _Viewimport

--- eCommerceApp/Controllers/HomeController.cs
+++ eCommerceApp/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var sliders = await _context.Sliders.ToListAsync();
+            return View(sliders);
         }
     }
 }
