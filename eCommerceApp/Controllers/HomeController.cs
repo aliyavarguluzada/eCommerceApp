8d1239dbe8b09c330f7e4f99fa983664e3ae081b
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-08-10 17:01:47 +0400

    Added _configuration to the scope (constructor)

--- eCommerceApp/Controllers/HomeController.cs
+++ eCommerceApp/Controllers/HomeController.cs
@@ -7,9 +7,12 @@ namespace eCommerceApp.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public HomeController(ApplicationDbContext context)
+        private readonly IConfiguration _configuration;
+        public HomeController(ApplicationDbContext context,
+            IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
         public async Task<IActionResult> Index()
         {
