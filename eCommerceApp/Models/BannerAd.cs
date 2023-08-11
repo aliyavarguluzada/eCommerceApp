100899edb637a75c6aedde4029fa8a6541ed0eb4
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-08-11 14:39:50 +0400

    Models BannerAd CategoryId and Category foreign key

--- eCommerceApp/Models/BannerAd.cs
+++ eCommerceApp/Models/BannerAd.cs
@@ -6,10 +6,12 @@
         public string Image { get; set; }
         public int BannerStatusId { get; set; }
 
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         public DateTime ExpireDate { get; set; }
         public DateTime? Created { get; set; }
 
         public DateTime? Updated { get; set; }
+
+        public Category Category { get; set; }
     }
 }
