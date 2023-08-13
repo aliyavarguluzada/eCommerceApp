332da649124fcf4ccfcde2086f5c2b8e5c2aed75
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-08-13 21:11:50 +0400

    Login Post hashing done

--- eCommerceApp/Controllers/AccountController.cs
+++ eCommerceApp/Controllers/AccountController.cs
@@ -1,13 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceApp.Enums;
+using eCommerceApp.Models;
+using eCommerceApp.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace eCommerceApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel request)
+        {
+            var user = await _context.Users.Where(c => c.Email == request.Email).FirstOrDefaultAsync();
+
+            if(user is null)
+            {
+                ModelState.AddModelError("", "Bele bir istifadeci yoxdu");
+                return View(request);
+            }
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                var buffer = Encoding.UTF8.GetBytes(request.Password);
+                var hash = sha256.ComputeHash(buffer);
+
+                if (!user.Password.SequenceEqual(hash))
+                {
+                    ModelState.AddModelError("","Passwords do not match");
+                    return RedirectToAction("Index","Home");
+                }
+            }
+
+                return View();
         }
 
         [HttpGet]
@@ -17,9 +57,28 @@ namespace eCommerceApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register()
+        public async Task<IActionResult> Register(RegisterModel request)
         {
-            return await RedirectToAction("");
+            var User = new User()
+            {
+                Name = request.Name,
+                Surname = request.Surname,
+                Email = request.Email,
+                RegisterDte = DateTime.Now,
+                UserRoleId = (int)UserRoleEnum.User,
+                Created = DateTime.Now,
+                Updated = DateTime.Now
+            };
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                var buffer = Encoding.UTF8.GetBytes(request.Password);
+                var hash = sha256.ComputeHash(buffer);
+
+                User.Password = hash;
+            }
+            await _context.AddAsync(User);
+            await _context.SaveChangesAsync();
+                return RedirectToAction("Login","Account");
         }
     }
 
---
+++ eCommerceApp/Enums/UserRoleEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceApp.Enums
+{
+    public enum UserRoleEnum
+    {
+        User = 10,
+        Admin = 20
+    }
+}
