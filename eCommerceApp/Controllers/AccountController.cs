09d8616569fbb8ef3511358695827211b4b6d24d
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-08-13 14:29:36 +0400

    Added Login Action to the Account Controller. Login page can now be accessed

---
+++ eCommerceApp/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+
+}
