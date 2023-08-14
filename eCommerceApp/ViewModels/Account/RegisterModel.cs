65df83f8839523c141cc5da7159e854d2f805443
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-08-14 11:55:21 +0400

    Added Data annotations to the RegisterModel Class

--- eCommerceApp/ViewModels/Account/RegisterModel.cs
+++ eCommerceApp/ViewModels/Account/RegisterModel.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace eCommerceApp.ViewModels.Account
 {
     public class RegisterModel
     {
         [Required(ErrorMessage ="Ad bos qala bilmez")]
+        [MaxLength(15,ErrorMessage ="Ad 15 den kicik olmalidi")]
+        [MinLength(3,ErrorMessage ="Ad 3 den kicik ola bilmez")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Soyad bos qala bilmez")]
+        [MaxLength(15, ErrorMessage = "Ad 15 den kicik olmalidi")]
+        [MinLength(3, ErrorMessage = "Ad 3 den kicik ola bilmez")]
 
         public string Surname { get; set; }
 
@@ -19,8 +24,9 @@ namespace eCommerceApp.ViewModels.Account
 
         public string Password { get; set; }
 
-        [Required(ErrorMessage ="Tekrar sifre duzgun deyila")]
+        [Required(ErrorMessage ="Tekrar sifre duzgun deyil")]
         [Compare("Password",ErrorMessage="Tekrar sifre duzgun deyil")]
+
         public string ConfirmPassword { get; set; }
 
     }
